9ab5451f443c0384ba9a71a11aee471f5ef04d53
OksanaSam <oksana.o.samokhvalova@gmail.com>
2021-06-12 23:49:44 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import { hot } from "react-hot-loader";
+
+const App = () => (
+  <div className="App">
+    <h1>Hello</h1>
+  </div>
+);
+
+export default hot(module)(App);
